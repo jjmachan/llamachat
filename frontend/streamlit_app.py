0e25d9d88661e215138aafdd299e70001ee904f2
jjmachan <jamesjithin97@gmail.com>
2023-09-27 12:48:19 +0530

    made app with contexts and streaming

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -10,34 +10,38 @@ if "messages" not in st.session_state.keys():
         {"role": "assistant", "content": "How may I help you?"},
     ]
 
-# load llama index
-
-storage_context = StorageContext.from_defaults(persist_dir="../notebooks/storage")
-index = load_index_from_storage(storage_context)
-chat_engine = index.as_chat_engine()
-
-
-def generate_response(prompt):
-    response = chat_engine.chat(prompt)
-    return response.response
-
 
 # Display chat messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(
+        message["role"], avatar="👨‍💻" if message["role"] == "user" else "🦙"
+    ):
         st.write(message["content"])
 
 # User-provided prompt
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar="👨‍💻"):
         st.write(prompt)
+
+# load llama index
+storage_context = StorageContext.from_defaults(persist_dir="../notebooks/storage")
+index = load_index_from_storage(storage_context)
+chat_engine = index.as_chat_engine(chat_mode="condense_question")
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_response(prompt)
-            st.write(response)
-    message = {"role": "assistant", "content": response}
+    with st.chat_message("assistant", avatar="🦙"):
+        msg_placeholder = st.empty()
+        full_response = ""
+        streaming_response = chat_engine.stream_chat(prompt)
+        for token in streaming_response.response_gen:
+            full_response += token
+            msg_placeholder.markdown(full_response + "▌")
+        msg_placeholder.markdown(full_response)
+        context_txts = [n.text for n in streaming_response.source_nodes]
+        for i, c in enumerate(context_txts):
+            expander = st.expander(f"Ref [{i}]")
+            expander.markdown(c)
+    message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
