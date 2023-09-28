bd471822011c2f2bd197d6d3136274e395683114
jjmachan <jamesjithin97@gmail.com>
2023-09-28 18:08:25 +0530

    fix typo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ LLAMAINDEX_EVAL_MSG = (
 LLAMA_INTRO_MSG = """\
 hey there 👋
 
-I'm llama, your go to for everything at this year's [PyCon India Confrence](https://in.pycon.org/2023/). You can ask me everything about the sessions happening this weekend. I'm still learning so please be patient with me. 
+I'm llama, your go to for everything at this year's [PyCon India Conference](https://in.pycon.org/2023/). You can ask me everything about the sessions happening this weekend. I'm still learning so please be patient with me. 
 """
 
 
